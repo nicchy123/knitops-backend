f52b39d11e8a7c85ceecbf1305e72c9cda70958e
nicchy123 <nasirchy252@gmail.com>
2023-07-21 10:19:01 +0600

    test

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 2000;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.mqbvthc.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -16,35 +15,36 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-
 async function run() {
   try {
-    const toolsCollection = client.db("knitOps").collection("tools");
     client.connect();
+    const toolsCollection = client.db("knitOps").collection("tools");
+   
+    // root page
+   
+    app.get("/", async (req, res) => {
+        res.send("server running");
+      });
 
-    app.get("/", (req, res) => {
-      res.send("server running");
-    });
-
-    app.get("/tools", async(req, res)=>{
+    app.get("/tools", async (req, res) => {
       const query = {};
       const result = await toolsCollection.find(query).toArray();
-      console.log(result)
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.get("/tools/:id", async(req, res)=>{
+    app.get("/tools/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await toolsCollection.findOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await toolsCollection.findOne(query);
+      res.send(result);
+    });
 
+ 
   } finally {
   }
 }
-run().catch(console.dir);
+run().catch((error) => console.log(error));
 
 app.listen(port, () => {
-  console.log(`knitOps on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
